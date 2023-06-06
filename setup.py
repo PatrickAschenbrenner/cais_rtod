e101d25fd28e7da77d5776f1e82829005baf1e29
Patrick Aschenbrenner <patrick.aschenbrenner@student.uibk.ac.at>
2023-06-06 22:07:40 +0200

    package settings

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as requirements_file:
+    # Parse requirements.txt, ignoring any commented-out lines.
+    requirements = [line for line in requirements_file.read().splitlines()
+                    if not line.startswith('#')]
+
+setup(
+    author='Patrick Aschenbrenner',
+    author_email='patrick.aschenbrenner@student.uibk.ac.at',
+    name='cais_rtod',
+    version='0.0.1',
+    description='Real-time object detection for images in CAIS simulations.',
+    packages=['cais_rtod'],
+    package_dir={'': 'src'}
+)
