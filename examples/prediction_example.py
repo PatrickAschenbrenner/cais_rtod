0be393e39ceab9c4f8cf018fe37fcf0bd903dcf9
Patrick Aschenbrenner <patrick.aschenbrenner@student.uibk.ac.at>
2023-06-19 19:26:39 +0200

    typo

--- examples/prediction_example.py
+++ examples/prediction_example.py
@@ -3,14 +3,14 @@ from cais_rtod.detector import YOLOv3, YOLOv8, SVM
 
 this_directory = os.path.abspath('')
 
-img_hand = image_path_hand = os.path.join(this_directory, "data", "hand_example.jpg")
-img_no_hand = image_path_no_hand = os.path.join(this_directory, "data", "no_hand_example.jpg")
+img_hand = os.path.join(this_directory, "data", "hand_example.jpg")
+img_no_hand = os.path.join(this_directory, "data", "no_hand_example.jpg")
 
 # YOLOv3 example
 print("YOLOv3:")
 yolov3_detector = YOLOv3()
-prediction_hand = yolov3_detector.predict(image_path_hand)
-prediction_no_hand = yolov3_detector.predict(image_path_no_hand)
+prediction_hand = yolov3_detector.predict(img_hand)
+prediction_no_hand = yolov3_detector.predict(img_no_hand)
 if prediction_hand == 1:
     print("True positive!")
 else:
@@ -24,8 +24,8 @@ print("")
 # YOLOv8 example
 print("YOLOv8:")
 yolov8_detector = YOLOv8()
-prediction_hand = yolov8_detector.predict(image_path_hand)
-prediction_no_hand = yolov8_detector.predict(image_path_no_hand)
+prediction_hand = yolov8_detector.predict(img_hand)
+prediction_no_hand = yolov8_detector.predict(img_no_hand)
 if prediction_hand == 1:
     print("True positive!")
 else:
@@ -39,8 +39,8 @@ print("")
 # SVM example
 print("SVM:")
 svm_detector = SVM()
-prediction_hand = svm_detector.predict(image_path_hand)
-prediction_no_hand = svm_detector.predict(image_path_no_hand)
+prediction_hand = svm_detector.predict(img_hand)
+prediction_no_hand = svm_detector.predict(img_no_hand)
 if prediction_hand == 1:
     print("True positive!")
 else:
